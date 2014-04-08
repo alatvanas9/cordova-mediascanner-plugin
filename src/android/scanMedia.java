24655a6e84e4a33fd956151a7ea6b0f67d6f71cf
Bryce Millet <BMillet@alpha.com>
2014-04-08 09:54:42 -0700

    Added initial src and www files

---
+++ src/android/scanMedia.java
@@ -0,0 +1,47 @@
+package com.alpha.scanMedia;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+public class scanMedia extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!action.equals("mediaScanner")) {
+            return false;
+        }
+        try {
+            String absolutePath = args.getString(0);
+            if (absolutePath.startsWith("data:image")) {
+                absolutePath = absolutePath.substring(absolutePath.indexOf(',') + 1);
+            }
+
+            return this.mediaScanner(absolutePath, callbackContext);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+    }
+
+    private boolean mediaScanner(String absolutePath, CallbackContext callbackContext) throws InterruptedException, JSONException
+    {
+          Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+          //File f = new File(filename);
+
+          Uri contentUri = Uri.parse(absolutePath.toString());
+          mediaScanIntent.setData(contentUri);
+          System.out.println("from internal?" + contentUri);
+          this.cordova.getActivity().sendBroadcast(mediaScanIntent);
+          return true;
+    }
+}
---
+++ www/scanMedia.js
@@ -0,0 +1,24 @@
+(function() {
+            /* This increases plugin compatibility */
+            var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+
+            /**
+            * The Java to JavaScript Gateway 'magic' class 
+            */
+            function scanMedia(){ }
+
+
+            scanMedia.prototype.mediaScanner = function(string, win, fail) {
+                cordovaRef.exec(win, fail, "scanMedia", "mediaScanner", [string]);
+            };
+
+            cordovaRef.addConstructor(function() {
+                if (!window.plugins) {
+                    window.plugins = {};
+                }
+                if (!window.plugins.scanMedia) {
+                    window.plugins.scanMedia = new scanMedia();
+                }
+            });
+
+        })();
