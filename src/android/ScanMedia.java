6943fbb8cfcfe5ccb94d136d7093de568df1090b
Bryce Millet <BMillet@alpha.com>
2014-04-28 11:08:25 -0700

    Changed scanMedia to ScanMedia

--- src/android/ScanMedia.java
+++ src/android/ScanMedia.java
@@ -11,7 +11,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
-public class scanMedia extends CordovaPlugin {
+public class ScanMedia extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
--- www/ScanMedia.js
+++ www/ScanMedia.js
@@ -1,24 +1,24 @@
 (function() {
-            /* This increases plugin compatibility */
-            var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+    /* This increases plugin compatibility */
+    var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
 
-            /**
-            * The Java to JavaScript Gateway 'magic' class 
-            */
-            function scanMedia(){ }
+    /**
+    * The Java to JavaScript Gateway 'magic' class 
+    */
+    function ScanMedia(){ }
 
 
-            scanMedia.prototype.mediaScanner = function(string, win, fail) {
-                cordovaRef.exec(win, fail, "scanMedia", "mediaScanner", [string]);
-            };
+        ScanMedia.prototype.mediaScanner = function(string, win, fail) {
+            cordovaRef.exec(win, fail, "ScanMedia", "mediaScanner", [string]);
+        };
 
-            cordovaRef.addConstructor(function() {
-                if (!window.plugins) {
-                    window.plugins = {};
-                }
-                if (!window.plugins.scanMedia) {
-                    window.plugins.scanMedia = new scanMedia();
-                }
-            });
+        cordovaRef.addConstructor(function() {
+            if (!window.plugins) {
+                window.plugins = {};
+            }
+            if (!window.plugins.ScanMedia) {
+                window.plugins.ScanMedia = new ScanMedia();
+            }
+        });
 
-        })();
+    })();
